f40bb76a9441b45967b4e83672c16139253a1a82
Alex Farrill <alex.farrill@gmail.com>
2011-09-04 02:09:34 -0400

    use APPLICATION

--- lib/capistrano/auto_cron.rb
+++ lib/capistrano/auto_cron.rb
@@ -1,18 +1,18 @@
 Capistrano::Configuration.instance.load do
   namespace :auto_cron do
     task :publish, :roles => :publish do
-      run "cd #{release_path} && #{rake} TEMPLATE=publish RAILS_ENV=#{rails_env} auto_cron"
+      run "cd #{current_path} && rake TEMPLATE=publish APPLICATION=#{application} RAILS_ENV=#{rails_env} auto_cron"
     end
 
-    desc "Connects to each box individual and installs the specifed cron 
+    desc "Connects to each box individually and installs the specified cron 
           USAGE:: role :publish_cron, 'prilb', :crontab => 'cron' " 
     task :publish_crons, :roles => :publish_cron do
       find_servers_for_task( current_task ).each do |sd|
         establish_connections_to( sd )
         targets = []
         targets.push sessions[sd]
-    
-        Command.process( "cd #{release_path} && #{rake} TEMPLATE=#{sd.options[:crontab]} RAILS_ENV=#{rails_env} auto_cron", 
+
+        Command.process( "cd #{current_path} && rake TEMPLATE=#{sd.options[:crontab]} RAILS_ENV=#{rails_env} auto_cron", 
                           targets, options.merge(:logger => logger), &block)
 
         logger.debug "creating crontab:#{sd.options[:crontab]} on #{sd.host}"
