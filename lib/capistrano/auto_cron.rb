a236ddc741f5c7bef9af31ced3e6704169799500
Alex Farrill <alex.farrill@gmail.com>
2011-11-19 23:23:39 -0500

    fix

--- lib/capistrano/auto_cron.rb
+++ lib/capistrano/auto_cron.rb
@@ -1,17 +1,7 @@
 Capistrano::Configuration.instance.load do
   namespace :auto_cron do
-    desc "Connects to each box individually and installs the specifed cron 
-          USAGE:: role :publish_cron, 'prilb', :crontabs => %w( publish production ) " 
-    task :publish_crons, :roles => :publish_crons do
-      find_servers_for_task( current_task ).each do |sd|
-        establish_connections_to( sd )
-        targets = []
-        targets.push sessions[sd]
-        Command.process( "cd #{release_path} && #{rake} TEMPLATES=#{sd.options[:crontabs].join(",")} APPLICATION=#{application} RAILS_ENV=#{rails_env} auto_cron", 
-                          targets, options.merge(:logger => logger), &block)
-
-        logger.debug "creating crontabs: #{sd.options[:crontabs].join(", ")} on #{sd.host}"
-      end
+    task :publish, :roles => :publish do
+      run "cd #{release_path} && #{rake} TEMPLATES=#{fetch(:auto_cron_templates, %w( publish )).join(",")} APPLICATION=#{application} RAILS_ENV=#{rails_env} auto_cron"
     end
   end
 end
